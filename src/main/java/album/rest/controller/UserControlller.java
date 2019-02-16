be550870d81d31186951bcb6a782e4aee2907993
Claudiu Pop <claudiu.pop@ngi.systems>
2019-02-16 16:26:52 +0200

    Implement the register rest api

--- src/main/java/album/rest/controller/UserControlller.java
+++ src/main/java/album/rest/controller/UserControlller.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/user")
 public class UserControlller {
 
     @Autowired
@@ -16,34 +17,25 @@ public class UserControlller {
     @Autowired
     private PhotoDAO photoDAO;
 
-    @GetMapping("/Users")
-    public String retrieveAllUsers() {
-        Iterable<User> all = userDAO.findAll();
+    @PostMapping("/register")
+    public ResponseEntity add(@RequestBody User user) {
 
-        StringBuilder sb = new StringBuilder();
+        try {
+            System.out.println(user.getUsername());
+            userDAO.save(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (user.getId() != null) {
+                userDAO.delete(user.getId());
+            }
+            System.out.println("ERR Message: " + e.getMessage());
+            if (e.getMessage().contains("ConstraintViolationException")) {
+                return ResponseEntity.badRequest().body("Username-ul este deja folosit, va rugam sa folositi altul!");
+            }
+            return ResponseEntity.badRequest().build();
 
-        all.forEach(p -> sb.append(p.getName() + "<br>"));
+        }
+        return ResponseEntity.ok("{}");
 
-        return sb.toString();
-    }
-
-    @GetMapping("/Users/{id}")
-    public User retrieveUser(@PathVariable long id) throws Exception {
-        return null;
-    }
-
-    @DeleteMapping("/Users/{id}")
-    public void deleteUser(@PathVariable long id) {
-
-    }
-
-    @PostMapping("/Users/Photo/{photo}")
-    public ResponseEntity<Object> createUser(@RequestBody Photo photo) {
-        return null;
-    }
-
-    @PutMapping("/Users/{id}")
-    public ResponseEntity<Object> updateUser(@RequestBody User User, @PathVariable long id) {
-        return null;
     }
 }
